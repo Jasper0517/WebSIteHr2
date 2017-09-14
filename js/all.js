3b0539caa2cc3d28a78acfcc5d5e4e9bce8ec24a
Jasper <maple0517@gmail.com>
2017-09-14 15:01:21 +0800

    update

--- js/all.js
+++ js/all.js
@@ -4,7 +4,7 @@ content = document.getElementById("contentmain");
 contentShow.addEventListener('click',function(e){
     // if(contentShow.)
     if(content.className == "contentshow"){
-        content.className = content.className.replace("contentshow","");        
+        content.className = content.className.replace("contentshow","contentmain");        
     }else{
         content.className = "contentshow";
     }
